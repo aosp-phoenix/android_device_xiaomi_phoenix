7fc46b65fcea67181a220b5439bfdf658d24c2e2
Naruto-242 <Naruto242@proton.me>
2025-02-01 14:39:57 +0000

    phoenix: Initial aosPB bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,5 +5,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_phoenix.mk
+    $(LOCAL_DIR)/aospb_phoenix.mk
 
--- aospb_phoenix.mk
+++ aospb_phoenix.mk
@@ -12,10 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from phoenix device
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common aosPB stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_phoenix
+# Flags for aosPB
+CUSTOM_BUILDTYPE := OFFICIAL
+CUSTOM_MAINTAINER := Naruto242
+
+PRODUCT_NAME := aospb_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
