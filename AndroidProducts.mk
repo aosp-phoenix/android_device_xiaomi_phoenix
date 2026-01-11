6c75cab803fdb55661dd459e985711299434016a
Naruto-242 <naruto242@proton.me>
2026-01-11 05:01:11 +0000

    phoenix: PixelOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,5 +5,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_phoenix.mk
+    $(LOCAL_DIR)/custom_phoenix.mk
 
--- custom_phoenix.mk
+++ custom_phoenix.mk
@@ -12,10 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from phoenix device
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := lineage_phoenix
+PRODUCT_NAME := custom_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
